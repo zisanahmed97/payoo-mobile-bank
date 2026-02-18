861eec1a0cfb0daa49e84359f2e9f8f27527242b
zisanahmed97 <zisanahmed97@gmail.com>
2026-02-18 22:17:07 +0600

    created homepage cashout section and added cashout.js file

---
+++ script/cashout.js
@@ -0,0 +1,9 @@
+document.getElementById('cashout-btn').addEventListener('click',function(){
+    console.log('cashout working')
+    //1.get the agent number and validate
+    //2.get the amount, validate,convert to number
+    //3.get the current balance, validate, convert to number
+    //4.calculate the balance
+    //5-1. true:: show an alert > set balance
+    //5-2. false:: show an error> return
+})
