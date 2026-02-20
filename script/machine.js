5e215993e6eeae46803d7af46ac8563e95c4e2f1
zisanahmed97 <zisanahmed97@gmail.com>
2026-02-20 14:53:49 +0600

    added toggle on add-money and cashout button

--- script/machine.js
+++ script/machine.js
@@ -5,3 +5,16 @@ function getValueFromInput(id){
    console.log(id,value);
    return value;
 }
+
+
+
+
+function showOnly(id){
+   const addMoney = document.getElementById('add-money');
+   const cashOut =document.getElementById('cashout');
+   addMoney.classList.add('hidden');
+   cashOut.classList.add('hidden');
+
+   const selected = document.getElementById(id);
+   selected.classList.remove('hidden');
+}
